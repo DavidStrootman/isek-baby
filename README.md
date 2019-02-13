edd705826e76442d03115378a7f5c5346796ac17
David Strootman <David.strootman@gmail.com>
2019-02-13 16:37:47 +0100

    Update readme with usage and example

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # isek-baby
-Binding of Isaac-style dungeon layout generator written in Python
+Binding of Isaac-style dungeon layout generator written in Python  
+## Usage
+* Create an instance of FloorGenerator
+* Call generateFloor every time you want to create a new layout
+
+To print a preview, run isek-baby.py in python console
+## Example
+![output example 1](./docs/images/usage_example_1.png)  
--- isek-baby.py
+++ isek-baby.py
@@ -2,22 +2,23 @@ from FloorGenerator import FloorGenerator as FloorGen
 
 
 """
-PRINT FLOOR LAYOUT TO PYTHON TERMINAL
+PRINT FLOOR LAYOUT TO PYTHON CONSOLE
 """
-floorGen = FloorGen(0, 0, 40, 1)
+floorGen = FloorGen(starting_x=0, starting_y=0, passes=1)
 
 
-def print_grid():
-    floorGen.generate_floor()
+def print_grid(floor_size: int, grid_size: int):
+    floorGen.generate_floor(floor_size)
     grid = []
-    grid_size = 31
+
     for i in range(grid_size):
         for j in range(grid_size):
             # place 0, 0 in middle by subtracting half of grid size on x and y
             if [i - round(grid_size / 2), j - round(grid_size / 2)] in floorGen.floor.used_coordinates:
                 grid.append("\033[91m" + "[X]" + "\033[0m")
             else:
-                grid.append("   ")
+                grid.append("···")
+
     grid_string = ""
     for i in range(grid_size * grid_size):
         if i % grid_size == 0 and i != 0:
@@ -25,18 +26,12 @@ def print_grid():
         grid_string += grid[i].__str__()
     print(grid_string)
 
-    input_generation()
-    # for i in range(floorGen.room_count):
-    #     floorGen.floor.rooms[i][0]
+
+def prompt_input():
+    floor_size = int(input("Floor Size: "))
+    grid_size = int(input("Grid size to print floor on: "))
+
+    print_grid(floor_size, grid_size)
 
 
-def input_generation():
-    operation = input('"')
-
-    if 0 <= operation.__len__():
-        print_grid()
-    else:
-        exit()
-
-
-input_generation()
+prompt_input()
